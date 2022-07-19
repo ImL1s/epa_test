c41dc679501a7ff53fc9d3fea9a2bf846e39163d
tw_sam <sam@wsp.com>
2022-07-19 20:02:14 +0700

    add: app bar and with search feature

--- app/src/main/java/com/iml1s/epa/MainActivity.kt
+++ app/src/main/java/com/iml1s/epa/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.iml1s.epa
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.iml1s.epa.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -10,12 +14,35 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var menu: Menu
+
     private val shareViewModel by viewModel<ShareViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolBar)
         setContentView(binding.root)
 //        val navController = findNavController(R.id.fragment_container_view)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // data binding not support menu.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        this.menu = menu
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+        return true
     }
 }
