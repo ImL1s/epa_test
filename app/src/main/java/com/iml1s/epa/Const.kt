7c6ebb5b093ce0a0ccf8a892f0d0a93d8bf7c368
tw_sam <sam@wsp.com>
2022-07-20 19:10:29 +0700

    add: air quality threshold

--- app/src/main/java/com/iml1s/epa/Const.kt
+++ app/src/main/java/com/iml1s/epa/Const.kt
@@ -1,3 +1,4 @@
 package com.iml1s.epa
 
 const val TIMBER_DEBUG_TAG = "tag_iml1s"
+const val QUALITY_GOOD_THRESHOLD = 30
--- app/src/main/java/com/iml1s/epa/main/viewModel/EpaViewModel.kt
+++ app/src/main/java/com/iml1s/epa/main/viewModel/EpaViewModel.kt
@@ -2,6 +2,7 @@ package com.iml1s.epa.main.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.iml1s.epa.QUALITY_GOOD_THRESHOLD
 import com.iml1s.epa.TIMBER_DEBUG_TAG
 import com.iml1s.epa.main.model.AirData
 import com.iml1s.epa.main.model.Record
@@ -20,6 +21,7 @@ class EpaViewModel(private val epaRepository: EpaRepository) : ViewModel() {
             .onEach {
                 Timber.tag(TIMBER_DEBUG_TAG).d(it.toString())
                 _goodAirQualityList.value = it.map { rawData -> createAirData(rawData) }
+                    .filter { item -> isGoodAirQuality(item.quality) }
             }
             .catch { Timber.tag(TIMBER_DEBUG_TAG).e(it) }
             .launchIn(viewModelScope)
@@ -27,5 +29,9 @@ class EpaViewModel(private val epaRepository: EpaRepository) : ViewModel() {
 
     private fun createAirData(rawData: Record) = rawData.run {
         AirData(siteID, siteName, county, pm25, status)
+    }
+
+    private fun isGoodAirQuality(qualityString: String): Boolean {
+        return (qualityString.toIntOrNull() ?: return false) <= QUALITY_GOOD_THRESHOLD
     }
 }
